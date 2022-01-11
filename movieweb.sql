039cdcccc991580e715165b26205c70686c5157f
Tran Khanh <khalongtvh@gmail.com>
2022-01-11 10:29:51 +0700

    db

--- movieweb.sql
+++ movieweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 08, 2022 at 10:13 AM
+-- Generation Time: Jan 11, 2022 at 04:28 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -106,7 +106,10 @@ CREATE TABLE `episodes` (
 --
 
 INSERT INTO `episodes` (`id`, `movie_id`, `link_movie`, `episode`, `slug_episode`) VALUES
-(1, 21, 'https://www.youtube.com/embed/https://www.youtube.com/embed/MudJaFFMrTU', 'tập 1', 'tap-1');
+(1, 21, 'https://www.youtube.com/embed/MudJaFFMrTU', 'tập 1', 'tap-1'),
+(10, 21, 'https://www.youtube.com/embed/uNaSgCBiyTs', 's', 's'),
+(11, 21, 'https://www.youtube.com/embed/MudJaFFMrTU', 'tập 1', 'tap-1'),
+(12, 28, 'https://www.youtube.com/embed/uNaSgCBiyTs', 's', 's');
 
 -- --------------------------------------------------------
 
@@ -196,7 +199,7 @@ INSERT INTO `movies` (`id`, `title`, `description`, `status`, `image`, `category
 (23, 'f', 'f', 1, 'got8543.jpg', 7, 2, 2, 'f'),
 (24, 'a', 'a', 1, 'funny copy9921.jpeg', 7, 2, 2, 'a'),
 (25, 'v', 'v', 1, '041900.jpg', 7, 2, 2, 'v'),
-(28, 'd', 's', 1, 'sci-fi76.jpg', 7, 2, 2, 'd');
+(28, 'Resident Evil: Quỷ Dữ Trỗi Dậy', 'Resident Evil: Quỷ dữ trỗi dậy - Resident Evil: Welcome to Raccoon City là bộ phim thuộc thể loại kinh dị, hành động của Mỹ. Tác phẩm do đạo diễn Johannes Roberts chịu trách nhiệm chỉ đạo, với sự tham gia của các diễn viên Kaya Scodelario, Hannah John-Kamen, Robbie Amell, Tom Hopper, Avan jogia và Donal Logue. Bộ phim đưa chúng ta theo chân nhóm cảnh sát của thành phố Raccoon cùng các thành viên của đội cứu hộ và tác chiến đặc biệt. Họ bị mắc kẹt ở một thị trấn nhỏ và phải đối mặt với đợt bùng phát dịch Zombie lớn chưa từng thấy do tập đoàn Umbrella, một siêu công ty dược phẩm mờ ám đứng đằng sau thao túng. Cả nhóm đã phải lên kế hoạch và tìm mọi cách để có thể thoát khỏi thành phố Raccoon. Lúc mà cuộc hỗn loạn nổ ra, Claire Redfield (Kaya Scodelario) đã trở lại thị trấn để cảnh báo anh trai cô là Chris (Robbie Amell) rằng Umbrella là một tổ chức không hề tốt đẹp một chút nào. Và từ đó, chúng ta được theo dõi hành trình của những nhân vật quen thuộc bước ra từ trong series game nổi tiếng, gồm bộ đôi chị em Claire và Chris Redfield, cảnh sát tân binh Leon S. Kennedy (Avan Jogia), đặc vụ Jill Valentine (Hannah John-Kamen) và Albert Wesker (Tom Hopper), đối đầu với thủ lĩnh của Công ty Umbrella là William Birkin (Neal McDonough). Phần phim mới có nhiều điểm khác biệt so với thương hiệu phim Resident Evil của Paul W. S. Anderson, bản chuyển thể lần này của Roberts bám sát hơn so với bản gốc từ game. Và điều này cũng mang đến sự quen thuộc với các game thủ của trò chơi cũng như những ai yêu thích trò chơi này. Phần trò chơi đầu tiên của loạt game Resident Evil đã được gọi là kinh dị sinh tồn, và roberts đã triển khai bộ phim theo hướng như vậy, một bộ phim kinh dị sinh tồn với nhiều phân cảnh máu me trong phim. Tác phẩm lấy bối cảnh phim vào năm 1998, thì bộ phim cũng bày tỏ sự tôn trọng với hai trò chơi đầu tiên của loạt game. Kỹ xảo của Maxime Alexandre trong phim cũng mang đến một sự sắc nét và chân thật, đặc biệt là các hình ảnh Iconic, với những mảng màu đỏ bao trùm cả bộ phim, với một cơn mưa bất tận trông như máu. Khác với những bộ phim Resident Evil khác trước đây, Welcome to Raccoon City không lạm dụng nhiều về số lượng Zombie trong các cảnh phim. Thay vào đó, phim xây dựng sự kịch tính bằng cách tăng từ từ số lượng zombie khi các nhân vật cận kề hiểm nguy. Trong lúc Roberts làm rất tốt việc truyền tải bản chất của trò chơi vào câu chuyện chính trên màn ảnh, thì dàn nhân vật lại được xây dựng một cách khá khó chịu. Việc mọi người liên tục sử dụng các từ ngữ thô tục liên tục không làm cho câu chuyện trở nên thú vị mà lại khiến người xem cảm thấy khó chịu. Bên cảnh đó, những nhân vật trông như những kẻ ngốc, đặc biệt là các phân cảnh đối diện với các tình huống cực kỳ tồi tệ, nhưng họ lại đáp lại bằng ánh mắt vô hồn đầy bối rối. Tuy nhiên cốt truyện chặt chẽ đã phần nào gỡ gạc cho sự nhàm chán của nhân vật.', 1, '067062.jpg', 7, 2, 4, 'resident-evil-quy-du-troi-day');
 
 -- --------------------------------------------------------
 
@@ -250,7 +253,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'khalongtvh@gmail.com', NULL, '$2y$10$tt2Ze2se3EKB8riCQQcK/upnWa3c5DQu8xZrpbD7paIMot7gWUaPq', 'k0zPnZtuXHw6ZxIp878GAceSWdSSnZIbQpBiIZ09vtMrxKxNqfEB6zmoeyW8', '2022-01-02 09:14:35', '2022-01-02 09:14:35'),
+(1, 'admin', 'khalongtvh@gmail.com', NULL, '$2y$10$tt2Ze2se3EKB8riCQQcK/upnWa3c5DQu8xZrpbD7paIMot7gWUaPq', 'Rf9lP6NiCHJU6i4FD08R0TMKCwwN7FU8m9DGQKeNvM6lmoYpUrfQMwLN1MqT', '2022-01-02 09:14:35', '2022-01-02 09:14:35'),
 (2, 'admin', 'khalongtvh1@gmail.com', NULL, '$2y$10$tt2Ze2se3EKB8riCQQcK/upnWa3c5DQu8xZrpbD7paIMot7gWUaPq', 'V6uDTopBSOM484GQOQ0BZleEwbhcXQVrZznLMp9TKosojE10XgQmkI46KtV8', '2022-01-02 09:14:35', '2022-01-02 09:14:35');
 
 --
@@ -353,7 +356,7 @@ ALTER TABLE `countries`
 -- AUTO_INCREMENT for table `episodes`
 --
 ALTER TABLE `episodes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
